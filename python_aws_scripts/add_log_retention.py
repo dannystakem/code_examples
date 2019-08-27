7ca669774f504b070d923dc6763e9261987fdaec
Danny Stakem <dannystakem@gmail.com>
2019-08-27 20:41:26 +0100

    python script for adding a retention period for Cloudwatch logs

---
+++ python_aws_scripts/add_log_retention.py
@@ -0,0 +1,26 @@
+import boto3
+import argparse
+
+def addLogRetention(retention_length):
+    client = boto3.client('logs')
+    response = client.describe_log_groups()
+    log_group_list = []
+
+    while True :
+        log_group_list = log_group_list + [ x['logGroupName'] for x in response['logGroups'] ]
+        if 'nextToken' in response.keys():
+            response = client.describe_log_groups(nextToken = response['nextToken'])
+        else:
+            break
+
+    for group in log_group_list:
+        print('Updating log group {}'.format(group))
+        response = client.put_retention_policy(logGroupName=group, retentionInDays=retention_length)
+
+    print('\nUpdated {} log groups - Added log retention of {} days'.format(len(log_group_list),retention_length))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--length', type=int, help='Number of days to set Log Retention')
+    args = parser.parse_args()
+    addLogRetention(args.length)
